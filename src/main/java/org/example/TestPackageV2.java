58113ae3a82735eba2855e583f21f1f156806643
huytvdev <huy8895@gmail.com>
2023-11-21 22:34:00 +0700

    add code

---
+++ src/main/java/org/example/TestPackageV2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class TestPackageV2 {
+
+
+  public static void print() {
+    // Press Opt+Enter with your caret at the highlighted text to see how
+    // IntelliJ IDEA suggests fixing it.
+    System.out.printf("TestPackageV2 Hello and welcome!");
+  }
+}
